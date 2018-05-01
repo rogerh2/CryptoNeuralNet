6c91d1ef4c8f5d3ad8e8d0eb35efd7b2df815e62
rogerh2 <rogeh2@gmail.com>
2018-04-30 23:14:58 -0400

    Minor bug changes to allow it to work with minutes, particularly had to fix some variable types

--- ToyScripts/playground_v2.py
+++ ToyScripts/playground_v2.py
@@ -15,8 +15,7 @@ def get_nth_hr_block(test_data, start_time, n=48, time_unit='hours'):
         time_delta = timedelta(minutes=n)
 
     start_date_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S %Z')
-
-    data_block = test_data[(test_data.index >= start_time) & (test_data.index <= start_date_time + time_delta)]
+    data_block = test_data[(test_data.index >= start_date_time) & (test_data.index <= (start_date_time + time_delta))]
     return data_block
 
 def buy_trade_loop(x_point, y_point, m, max_iterations, iterable, delta, delta_end_point):
@@ -47,16 +46,16 @@ def find_trades(data_block, trade_data_frame=None, n=24, m=4, max_iterations=5, 
     buy_x_point = data_block.idxmin()[0]
 
     if time_unit == 'days':
-        time_delta = pd.Timedelta(days=n)
+        time_delta = pd.Timedelta(days=m)
         buy_sell_delta = pd.Timedelta(days=m)
     elif time_unit == 'hours':
-        time_delta = pd.Timedelta(hours=n)
+        time_delta = pd.Timedelta(hours=m)
         buy_sell_delta = pd.Timedelta(hours=m)
     else:
-        time_delta = pd.Timedelta(minutes=n)
+        time_delta = pd.Timedelta(minutes=m)
         buy_sell_delta = pd.Timedelta(minutes=m)
 
-    buy_x_point, buy_y_point, n_buy = buy_trade_loop(buy_x_point, buy_y_point, 1, max_iterations, data_block, time_delta, data_block.index[0] + time_delta)
+    buy_x_point, buy_y_point, n_buy = buy_trade_loop(buy_x_point, buy_y_point, 1, max_iterations, data_block, time_delta, data_block.index[-1])
     if buy_x_point > (np.max(data_block.index) - time_delta):
         return False
 
@@ -76,13 +75,13 @@ def find_trades(data_block, trade_data_frame=None, n=24, m=4, max_iterations=5, 
 
 #TODO create function to find all trades and return them in pandas dataframe (not just two moves like find_trades)
 
-test_data = DataSet("2018-04-25 18:00:00 EST", "2018-04-30 18:00:00 EST", days=1, google_list=['Etherium'], prediction_ticker='ltc', bitinfo_list=['btc', 'eth', 'ltc'], time_units='hours')
+test_data = DataSet("2018-04-29 18:00:00 EST", "2018-04-30 18:00:00 EST", days=1, google_list=['Etherium'], prediction_ticker='ltc', bitinfo_list=['btc', 'eth', 'ltc'], time_units='minutes')
 test_data.create_arrays(type='price')
 df = test_data.final_table.LTC_open
 
-data_block = get_nth_hr_block(df, "2018-04-27 14:00:00 EST")
+data_block = get_nth_hr_block(df, "2018-04-30 12:00:00 EST", n=120, time_unit='minutes')
 data_frame_block = data_block.to_frame()
-final_data_frame = find_trades(data_frame_block)
+final_data_frame = find_trades(data_frame_block, n=60, m=10, max_iterations=5, time_unit='minutes')
 print(final_data_frame)
-#df.plot()
+#data_frame_block.plot()
 #plt.show()
