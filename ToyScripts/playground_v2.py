169feeb82897e4b4ccebe3fb66f048cc71d4f48b
rogerh2 <rogeh2@gmail.com>
2018-05-01 22:29:08 -0400

    Add method to turn the the buy and sell dataframe into two columns for predictions

--- ToyScripts/playground_v2.py
+++ ToyScripts/playground_v2.py
@@ -66,7 +66,7 @@ def find_trades(data_block, trade_data_frame=None, n=4, max_iterations=5, time_u
     buy_x_point, buy_y_point, n_buy = buy_trade_loop(buy_x_point, buy_y_point, n_buy, max_iterations, data_after_buy,
                                                      buy_sell_delta, sell_x_point)
     sell_x_point, sell_y_point, n_sell = sell_trade_loop(sell_x_point, sell_y_point, max_iterations, data_after_buy, buy_sell_delta, buy_x_point)
-    df = pd.DataFrame([[buy_x_point, buy_y_point, sell_x_point, sell_y_point]], columns=['Buy X', 'Buy Y', 'Sell X', 'Sell Y'])
+    df = pd.DataFrame([[buy_x_point, buy_y_point, sell_x_point, sell_y_point]], columns=['Buy_X', 'Buy_Y', 'Sell_X', 'Sell_Y'])
     if trade_data_frame is not None:
         final_data_frame = trade_data_frame.append(df, ignore_index=True)
     else:
@@ -98,7 +98,7 @@ def find_all_trades(data_frame, time_unit='hours', n_0=48, m=6, max_iterations=5
         data_block = get_nth_hr_block(df, start_time_str + ' EST', n=n, time_unit=time_unit)
         data_frame_block = data_block.to_frame()
         current_block_trades, current_buy, current_sell = find_trades(data_frame_block, trade_data_frame=block_trades, n=m, max_iterations=max_iterations, time_unit=time_unit)
-        if (current_sell - current_buy) < 0.005*current_buy or type(current_block_trades) is not pd.core.frame.DataFrame:
+        if (current_sell - current_buy) < 0.005*current_buy or type(current_block_trades) is not pd.DataFrame:
             n = n + n_0
             time_delta = timedelta_strunits(n, time_unit)
             search_range = search_range + time_delta
@@ -110,6 +110,40 @@ def find_all_trades(data_frame, time_unit='hours', n_0=48, m=6, max_iterations=5
 
     return  block_trades
 
+def create_single_prediction_column(price_data_frame, trade_data_frame, time_units='hours'):
+
+    if time_units == 'days':
+        norm_constant = 24*3600
+    elif time_units == 'hours':
+        norm_constant = 3600
+    else:
+        norm_constant = 60
+
+    loop_len = price_data_frame.count()
+    trade_ind = 0
+    trade_time = trade_data_frame[trade_ind]
+    time_arr = None
+    for price_ind in range(0, loop_len):
+        price_time = price_data_frame.index[price_ind]
+        if price_time > trade_time:
+            trade_ind += 1
+            if trade_ind >= trade_data_frame.count():
+                return time_arr
+            trade_time = trade_data_frame[trade_ind]
+
+        time_to_trade = (trade_time - price_time).total_seconds()/norm_constant
+
+        if time_arr is None:
+            time_arr = np.array([time_to_trade])
+        else:
+            time_arr = np.vstack((time_arr, np.array([time_to_trade])))
+
+def  create_prediction_columns(price_data_frame, buy_sell_data_frame, time_units='hours'):
+    buy_column = create_single_prediction_column(price_data_frame, buy_sell_data_frame.Buy_X, time_units)
+    sell_column = create_single_prediction_column(price_data_frame, buy_sell_data_frame.Sell_X, time_units)
+    prediction_columns = np.hstack((buy_column, sell_column[0:len(buy_column)]))
+    return prediction_columns
+
 test_data = DataSet("2018-04-30 21:00:00 EST", "2018-05-01 20:30:00 EST", days=1, google_list=['Etherium'], prediction_ticker='ltc', bitinfo_list=['btc', 'eth', 'ltc'], time_units='minutes')
 test_data.create_arrays(type='price')
 df = test_data.final_table.LTC_open
@@ -119,7 +153,8 @@ df = test_data.final_table.LTC_open
 # final_data_frame = find_trades(data_frame_block, n=10, max_iterations=5, time_unit='minutes')
 
 final_data_frame = find_all_trades(df, time_unit='minutes', n_0=60, m=15, max_iterations=5)
-print(final_data_frame)
-print(final_data_frame['Sell Y'].sum() - final_data_frame['Buy Y'].sum())
+arr = create_prediction_columns(df, final_data_frame, time_units='minutes')
+
+print(arr)
 #data_frame_block.plot()
 #plt.show()
