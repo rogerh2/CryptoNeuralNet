03b2542c69a42191e4ff574e57859a9a9432412d
rogerh2 <rogeh2@gmail.com>
2019-01-29 22:59:22 -0500

    Added unittests for the BackTestExchange class in the BackTest file. Also fixed errors in the aforementioned class.
    all tests are passing!

--- CryptoBot/BackTest.py
+++ CryptoBot/BackTest.py
@@ -15,42 +15,63 @@ class BackTestExchange():
         self.order_books = pd.read_csv(order_book_path)
         #self.model = keras.models.load_model(model_path)
 
-    def get_top_ask(self, ind):
-        top_ask = self.order_books['60'].values[ind]
-        return top_ask
-
-    def get_top_bid(self, ind):
-        top_ask = self.order_books['0'].values[ind]
-        return top_ask
+    def get_top_order(self, ind, side):
+        if side == 'asks':
+            col = '60'
+        elif side == 'bids':
+            col = '0'
+        top_order = self.order_books.iloc[[ind]][col].values[0]
+        return top_order
 
     def get_current_book(self, ind):
-        current_order_book_row = self.order_books[ind]
+        current_order_book_row = self.order_books.iloc[[ind]]
         current_order_book_row = current_order_book_row.drop(['ts'], axis=1)
         return current_order_book_row
 
     def place_order(self, price, side, size, ind):
-        if side == 'ask':
-            top_order = self.get_top_ask(ind)
+
+        if side == 'asks':
             coeff = 1
-        elif side == 'bid':
-            top_order = self.get_top_bid(ind)
+            opposing_side = 'bids'
+        elif side == 'bids':
             coeff = -1
+            opposing_side = 'asks'
         else:
             raise ValueError(side + ' is not a valid orderbook side')
+        top_opposing_order = self.get_top_order(ind, opposing_side)
 
-        if coeff*price >= top_order: # This ensures the order is only placed onto the appropiate order book side
+        if coeff*price >= coeff*top_opposing_order: # This ensures the order is only placed onto the appropiate order book side
             this_side_orders = self.orders[side]
-            existing_ids = np.array(list(this_side_orders.keys()))
-            new_order_id = np.max(existing_ids) + 1
-            this_side_orders[new_order_id] = {'size':size, 'price':price, 'settled':False}
+            if len(this_side_orders) > 0:
+                existing_ids = np.array(list(this_side_orders.keys()))
+                new_order_id = np.max(existing_ids) + 1
+            else:
+                new_order_id = 0
+            this_side_orders[new_order_id] = {'size':size, 'price':price, 'filled':False}
 
-    def remove_order(self, order_id):
-        self.orders.pop(order_id)
+    def update_fill_status(self, ind):
+        sides = ['bids', 'asks']
+
+        for side_ind in [0, 1]:
+            # This loop cycles through the bids and asks books and fills any orders that are on the wrong book
+            side = sides[side_ind]
+            opposing_side = sides[not side_ind]
+            coeff = (-1)**(side_ind)
+            top_opposing_order = self.get_top_order(ind, opposing_side)
+
+            for order in self.orders[side].values():
+                if (not order['filled']) and (coeff*order['price'] >= coeff*top_opposing_order):
+                    order['filled'] = True
+
+    def remove_order(self, side, order_id):
+        self.orders[side].pop(order_id)
 
 
 class BackTestPortfolio():
-    value=100
+    value = {'USD':100, 'BTC':0}
 
     def __init__(self, order_book_path):
         self.exchange = BackTestExchange(order_book_path)
 
+    def place_bid(self):
+        temp = True
---
+++ CryptoBot/CryptoBotUnitTests/BackTestExchangeUnitTests.py
@@ -0,0 +1,113 @@
+import unittest
+import os
+import pandas as pd
+import numpy as np
+import pickle
+from pathlib import Path
+from CryptoBot.BackTest import BackTestExchange
+import keras
+from datetime import datetime
+from datetime import timedelta
+
+class BackTestExchangeTestCase(unittest.TestCase):
+    test_obj = BackTestExchange(
+        '/Users/rjh2nd/PycharmProjects/CryptoNeuralNet/CryptoBot/CryptoBotUnitTests/UnitTestData/order_book_for_back_test_unit_tests.csv')
+
+    def test_can_return_top_order(self):
+        top_bid = self.test_obj.get_top_order(0, 'bids')
+        top_ask = self.test_obj.get_top_order(0, 'asks')
+
+        self.assertEqual(top_bid, 100, 'Incorrect bid')
+        self.assertEqual(top_ask, 101, 'Incorrect ask')
+
+    def test_returns_current_order_book(self):
+        test_book = self.test_obj.get_current_book(3)
+        self.assertEqual(test_book['0'].values[0], 102)
+        self.assertEqual(test_book['60'].values[0], 105)
+
+    def test_places_bid_with_correct_price_and_size_and_removes_order(self):
+        price = 100
+        size = 1
+        side = 'bids'
+        self.test_obj.place_order(price, 'bids', size, 3)
+
+        self.assertEqual(price, self.test_obj.orders[side][0]['price'], 'Incorrect price')
+        self.assertEqual(size, self.test_obj.orders[side][0]['size'], 'Incorrect size')
+
+        self.test_obj.remove_order(side, 0)
+        self.assertEqual(len(self.test_obj.orders[side]), 0, 'Did not remove order')
+
+    def test_places_ask_with_correct_price_and_size_and_removes_order(self):
+        price = 105.1
+        size = 1
+        side = 'asks'
+        self.test_obj.place_order(price, side, size, 3)
+
+        self.assertEqual(price, self.test_obj.orders[side][0]['price'], 'Incorrect price')
+        self.assertEqual(size, self.test_obj.orders[side][0]['size'], 'Incorrect size')
+
+        self.test_obj.remove_order(side, 0)
+        self.assertEqual(len(self.test_obj.orders[side]), 0, 'Did not remove order')
+
+    def test_will_not_place_bid_on_ask_book(self):
+        price = 110
+        size = 1
+        side = 'bids'
+        self.test_obj.place_order(price, 'bids', size, 3)
+
+        self.assertEqual(len(self.test_obj.orders[side]), 0, 'Placed bid on ask book')
+
+    def test_will_not_place_ask_on_ask_book(self):
+        price = 90
+        size = 1
+        side = 'asks'
+        self.test_obj.place_order(price, 'asks', size, 3)
+
+        self.assertEqual(len(self.test_obj.orders[side]), 0, 'Placed ask on bid book')
+
+    def test_can_update_fill_status_for_bids(self):
+        price = 102.5
+        size = 1
+        side = 'bids'
+        self.test_obj.place_order(price, side, size, 1)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled')
+        self.test_obj.update_fill_status(2)
+        self.assertTrue(self.test_obj.orders[side][0]['filled'], 'Bid did not fill')
+
+    def test_can_update_fill_status_for_asks(self):
+        price = 103
+        size = 1
+        side = 'asks'
+        self.test_obj.place_order(price, side, size, 3)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled')
+        self.test_obj.update_fill_status(4)
+        self.assertTrue(self.test_obj.orders[side][0]['filled'], 'Ask did not fill')
+
+    def test_does_not_incorrectly_update_fill_status_for_asks(self):
+        price = 120
+        size = 1
+        side = 'asks'
+        self.test_obj.place_order(price, side, size, 3)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled')
+        self.test_obj.update_fill_status(4)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled on update')
+
+    def test_does_not_incorrectly_update_fill_status_for_bids(self):
+        price = 90
+        size = 1
+        side = 'bids'
+        self.test_obj.place_order(price, side, size, 2)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled')
+        self.test_obj.update_fill_status(3)
+        self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled on update')
+
+    def tearDown(self):
+        if len(self.test_obj.orders['asks']) > 0:
+            self.test_obj.remove_order('asks', 0)
+
+        if len(self.test_obj.orders['bids']) > 0:
+            self.test_obj.remove_order('bids', 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
