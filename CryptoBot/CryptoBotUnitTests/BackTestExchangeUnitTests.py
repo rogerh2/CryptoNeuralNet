1d829656befcfeb7ef95270be0442f57a63fc764
rogerh2 <rogeh2@gmail.com>
2019-03-09 15:21:05 -0500

    Updated BackTestExchangeUnitTests. The method for tracking time had been changed in the BackTestExchange class so the unit tests needed to be updated to reflect that

--- CryptoBot/CryptoBotUnitTests/BackTestExchangeUnitTests.py
+++ CryptoBot/CryptoBotUnitTests/BackTestExchangeUnitTests.py
@@ -6,14 +6,16 @@ class BackTestExchangeTestCase(unittest.TestCase):
         '/Users/rjh2nd/PycharmProjects/CryptoNeuralNet/CryptoBot/CryptoBotUnitTests/UnitTestData/order_book_for_back_test_unit_tests.csv')
 
     def test_can_return_top_order(self):
-        top_bid = self.test_obj.get_top_order(0, 'bids')
-        top_ask = self.test_obj.get_top_order(0, 'asks')
+        self.test_obj.time = 0
+        top_bid = self.test_obj.get_top_order('bids')
+        top_ask = self.test_obj.get_top_order('asks')
 
         self.assertEqual(top_bid, 100, 'Incorrect bid')
         self.assertEqual(top_ask, 101, 'Incorrect ask')
 
     def test_returns_current_order_book(self):
-        test_book = self.test_obj.get_current_book(3)
+        self.test_obj.time = 3
+        test_book = self.test_obj.get_current_book()
         self.assertEqual(test_book['0'].values[0], 102)
         self.assertEqual(test_book['60'].values[0], 105)
 
@@ -21,7 +23,8 @@ class BackTestExchangeTestCase(unittest.TestCase):
         price = 100
         size = 1
         side = 'bids'
-        self.test_obj.place_order(price, 'bids', size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, 'bids', size)
 
         self.assertEqual(price, self.test_obj.orders[side][0]['price'], 'Incorrect price')
         self.assertEqual(size, self.test_obj.orders[side][0]['size'], 'Incorrect size')
@@ -33,7 +36,8 @@ class BackTestExchangeTestCase(unittest.TestCase):
         price = 105.1
         size = 1
         side = 'asks'
-        self.test_obj.place_order(price, side, size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, side, size)
 
         self.assertEqual(price, self.test_obj.orders[side][0]['price'], 'Incorrect price')
         self.assertEqual(size, self.test_obj.orders[side][0]['size'], 'Incorrect size')
@@ -45,7 +49,8 @@ class BackTestExchangeTestCase(unittest.TestCase):
         price = 110
         size = 1
         side = 'bids'
-        self.test_obj.place_order(price, 'bids', size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, 'bids', size)
 
         self.assertEqual(len(self.test_obj.orders[side]), 0, 'Placed bid on ask book')
 
@@ -53,7 +58,8 @@ class BackTestExchangeTestCase(unittest.TestCase):
         price = 90
         size = 1
         side = 'asks'
-        self.test_obj.place_order(price, 'asks', size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, 'asks', size)
 
         self.assertEqual(len(self.test_obj.orders[side]), 0, 'Placed ask on bid book')
 
@@ -61,36 +67,44 @@ class BackTestExchangeTestCase(unittest.TestCase):
         price = 102.5
         size = 1
         side = 'bids'
-        self.test_obj.place_order(price, side, size, 1)
+        self.test_obj.time = 1
+        self.test_obj.place_order(price, side, size)
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled')
-        self.test_obj.update_fill_status(2)
+        self.test_obj.time = 2
+        self.test_obj.update_fill_status()
         self.assertTrue(self.test_obj.orders[side][0]['filled'], 'Bid did not fill')
 
     def test_can_update_fill_status_for_asks(self):
         price = 103
         size = 1
         side = 'asks'
-        self.test_obj.place_order(price, side, size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, side, size)
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled')
-        self.test_obj.update_fill_status(4)
+        self.test_obj.time = 4
+        self.test_obj.update_fill_status()
         self.assertTrue(self.test_obj.orders[side][0]['filled'], 'Ask did not fill')
 
     def test_does_not_incorrectly_update_fill_status_for_asks(self):
         price = 120
         size = 1
         side = 'asks'
-        self.test_obj.place_order(price, side, size, 3)
+        self.test_obj.time = 3
+        self.test_obj.place_order(price, side, size)
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled')
-        self.test_obj.update_fill_status(4)
+        self.test_obj.time = 4
+        self.test_obj.update_fill_status()
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Ask auto filled on update')
 
     def test_does_not_incorrectly_update_fill_status_for_bids(self):
         price = 90
         size = 1
         side = 'bids'
-        self.test_obj.place_order(price, side, size, 2)
+        self.test_obj.time = 2
+        self.test_obj.place_order(price, side, size)
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled')
-        self.test_obj.update_fill_status(3)
+        self.test_obj.time = 3
+        self.test_obj.update_fill_status()
         self.assertFalse(self.test_obj.orders[side][0]['filled'], 'Bid auto filled on update')
 
     def tearDown(self):
