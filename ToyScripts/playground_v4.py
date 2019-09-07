a1636af89c08d81f3bad04b43b133aa8aa1c78e9
rogerh2 <rogeh2@gmail.com>
2019-09-07 09:06:52 -0400

    Created a script that can reverse engineer the coefficients for the unforced harmonic oscillator from the data. Going to the forced equation next

--- ToyScripts/playground_v4.py
+++ ToyScripts/playground_v4.py
@@ -39,38 +39,40 @@ def piece_wise_fit_eval(coeffs, x=None):
     return fit, [start_ind, stop_ind]
 
 def calculate_coefficients_for_second_order(polynomial_coefficients, F):
-    # y'' + gamma * y' + omega * y = F
+    # y'' + zeta * omega * y' + omega^2 * y = F
     y = np.flip(polynomial_coefficients[-4::], 0)
-    denominator = y[2]*y[0] - y[1]**2
-    gamma_numerator = -F*y[1] + y[2]*y[1] + 2*y[3]*y[0]
-    omega_numerator = y[2]*F - y[2]**2 - 2*y[3]*y[1]
-    gamma = gamma_numerator / denominator
-    omega = omega_numerator / denominator
+    denominator = 2 * y[1]**2 - 4 * y[2] * y[0]
 
-    return gamma, omega
+    zeta_numerator = -4 * y[2] * y[1] + 12 * y[0] * y[3]
+
+    omega_squared_numerator = 8*y[2]**2 - 12 * y[1] * y[3]
+
+    omega = np.sqrt(omega_squared_numerator / denominator)
+    zeta = zeta_numerator / ( 2 * omega * denominator )
+
+    return zeta, omega
 
 if __name__ == "__main__":
     # cc = CryptoCompare(date_from='2019-08-29 14:39:00 Eastern Standard Time')
     # data = cc.minute_price_historical('ETH')['ETH_close'].values
     decay_rate = 0.05
     freq = np.sqrt(0.9975)
-    t = np.arange(0, 30*np.pi, 0.1)
+    t = np.arange(0, 30*np.pi, 0.01)
     c = np.pi/2
     data = np.exp(-decay_rate*t)*np.sin(freq*t + c)
-    coeff = construct_piecewise_polynomial_for_data(data, 4, x=t)
+    coeff = construct_piecewise_polynomial_for_data(data, 10, x=t)
     poly_fit, start_stop = piece_wise_fit_eval(coeff, x=t)
     fit_t = t[start_stop[0]:start_stop[1]]
     plt.plot(fit_t, data[start_stop[0]:start_stop[1]], 'b')
     plt.plot(fit_t, poly_fit, 'rx')
     plt.show()
     for i in range(1, len(coeff)):
-        gamma, omega = calculate_coefficients_for_second_order(coeff[i][0], 0)
-        print(gamma, omega)
-        fit_decay = gamma / 2
-        z = gamma / (2 * np.sqrt(omega))
-        if np.isnan(z):
+        zeta, omega = calculate_coefficients_for_second_order(coeff[i][0], 0)
+        print(zeta, omega)
+        fit_decay = zeta * omega
+        if np.isnan(zeta):
             continue
-        fit_freq = np.sqrt(omega) * np.sqrt(1 - z**2)
+        fit_freq = omega * np.sqrt(1 - zeta ** 2)
         fit = np.exp(-fit_decay*fit_t)*np.sin(fit_freq*fit_t + c)
         plt.plot(fit_t, data[start_stop[0]:start_stop[1]],'b')
         plt.plot(fit_t, fit,'rx')
