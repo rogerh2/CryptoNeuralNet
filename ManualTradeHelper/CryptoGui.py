3328ec97fa7879eec183057d4d32e5547e970112
rogerh2 <rogeh2@gmail.com>
2020-05-08 10:38:50 -0400

    Added multithreading to update plot while loop is running

--- ManualTradeHelper/CryptoGui.py
+++ ManualTradeHelper/CryptoGui.py
@@ -6,27 +6,36 @@ from CryptoBot.SpreadBot import Product, Wallet, LiveRunSettings, CombinedPortfo
 from CryptoBot.CryptoBot_Shared_Functions import num2str
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from threading import Thread
+from time import sleep
 
-# Here we add a class to represent the links for an individual currency
+# Here we add a class to represent the window for an individual currency
 class CurrencyGui(Frame):
 
-    def __init__(self, master, controller, wallet, bot, sym):
+    def __init__(self, master, controller, bot, sym):
         # parameters that you want to send through the Frame class.
         Frame.__init__(self, master)
         self.master = master
         self.controller = controller
-        self.wallet = wallet
+        self.wallet = bot.portfolio.wallets[sym]
         self.bot = bot
         self.tracker = 0
         self.sym = sym
 
-        self.plot()#self.showImg("/Users/rjh2nd/PycharmProjects/CryptoNeuralNet/test.png")
+        # Plot the initial data
+        self.raw_line, self.predict_line, self.previous_line, self.canvas, self.ax = self.plot()#self.showImg("/Users/rjh2nd/PycharmProjects/CryptoNeuralNet/test.png")
+
+        # Create inputs for prices
         self.price_entry = self.display_text_entry('Price', 1, 0)
         self.spread_entry = self.display_text_entry('Spread', 2, 0)
+
+        # Create buttons to trade
         self.buy_button = Button(master, text='Buy', command=self.buy)
         self.buy_button.grid(row=3, column=1)
         self.sell_button = Button(master, text='Sell', command=self.sell)
         self.sell_button.grid(row=3, column=2)
+
+        # The refresh button updates the window information
         self.refresh_button = Button(master, text='Refresh', command=self.refresh_portfolio)
         self.refresh_button.grid(row=3, column=3)
 
@@ -55,17 +64,33 @@ class CurrencyGui(Frame):
         prediction = self.bot.predictions[self.sym]
         raw_data = self.bot.raw_data[self.sym]
         reversed_prediction = self.bot.reversed_predictions[self.sym]
+        zero = raw_data[-1]
 
-        axis.plot(np.arange(0, len(raw_data)), raw_data)
-        axis.plot(np.arange(len(raw_data), len(prediction) + len(raw_data)), prediction)
-        axis.plot(np.arange(len(raw_data) - len(reversed_prediction), len(raw_data)), reversed_prediction)
+        raw_line, = axis.plot(np.arange(0, len(raw_data)), raw_data/zero)
+        predict_line, = axis.plot(np.arange(len(raw_data), len(prediction) + len(raw_data)), prediction/zero)
+        previous_line, = axis.plot(np.arange(len(raw_data) - len(reversed_prediction), len(raw_data)), reversed_prediction/zero)
         # plt.title(self.sym + ' Prediction')
         # plt.xlabel('Time (min)')
         # plt.ylabel('Price ($)')
 
-        chart_type = FigureCanvasTkAgg(figure, master=self.master)
-        chart_type.show()
-        chart_type.get_tk_widget().grid(row=0, columnspan=4)
+        canvas = FigureCanvasTkAgg(figure, master=self.master)
+        canvas.show()
+        canvas.get_tk_widget().grid(row=0, columnspan=4)
+
+        return raw_line, predict_line, previous_line, canvas, axis
+
+    def update_plot(self):
+        zero = self.bot.raw_data[self.sym][-1]
+        raw_data = self.bot.raw_data[self.sym]/zero
+        prediction = self.bot.predictions[self.sym]/zero
+        reversed_prediction = self.bot.reversed_predictions[self.sym]/zero
+        self.previous_line.set_ydata(reversed_prediction)
+        self.predict_line.set_ydata(prediction)
+        self.raw_line.set_ydata(raw_data)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.canvas.draw()
+        plt.pause(0.01)
 
     def get_price_and_spread(self):
         price = self.price_entry.get()
@@ -94,8 +119,9 @@ class CurrencyGui(Frame):
             print('Price: ' + num2str(price))
 
     def refresh_portfolio(self):
-        Label(self.master, text='Sym in Portfolio: ' + num2str(self.tracker)).grid(row=1, column=3)
+        Label(self.master, text= self.sym + ' in Portfolio: ' + num2str(self.wallet.get_amnt_available('sell'))).grid(row=1, column=3)
         Label(self.master, text='Average Buy Price: ' + num2str(self.tracker)).grid(row=2, column=3)
+        self.update_plot()
 
 
 
@@ -167,7 +193,11 @@ class Window(Frame):
     def client_exit(self):
         exit()
 
-
+def sleepy_predict(bot, root):
+    sleep(10)
+    print('starting next prediction')
+    bot.predict(verbose_on=True)  #bot.predictions['BTC'] += 100  #
+    root.update()
 
 if __name__ == "__main__":
     # root window created. Here, that would be the only window, but
@@ -182,7 +212,12 @@ if __name__ == "__main__":
     passphrase_input = input('What is the passphrase? ')
     psmbot = PSMPredictBot(api_input, secret_input, passphrase_input)
     psmbot.predict(verbose_on=True)
-    app = CurrencyGui(root, None, None, psmbot, 'BTC')#Window(root)
+    # psmbot.predictions['BTC'] = np.arange(0, 30)
+    # psmbot.reversed_predictions['BTC'] = np.arange(-30, 0)
+    # psmbot.raw_data['BTC'] = np.arange(-480, 0)
+    app = CurrencyGui(root, None, psmbot, 'BTC')#Window(root)
 
     # mainloop
+    predict_thread = Thread(target=sleepy_predict, args=(psmbot,root))
+    predict_thread.start()
     root.mainloop()
